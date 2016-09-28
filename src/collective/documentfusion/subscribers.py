d541619d40865159ca0c76b5cc77664151f6c3d7
Thomas Desvenain <thomas.desvenain@gmail.com>
2016-09-28 14:33:21 +0200

    Get back pdf transformation

--- src/collective/documentfusion/subscribers.py
+++ src/collective/documentfusion/subscribers.py
@@ -7,7 +7,6 @@ def refresh(obj, event=None):
     if IMergeDocumentFusion.providedBy(obj):
         return merge_document(obj)
 
-    # target_extension = IPDFGeneration.providedBy(obj) and 'pdf' or None
-    target_extension = 'odt'
+    target_extension = IPDFGeneration.providedBy(obj) and 'pdf' or None
     make_fusion = IDocumentFusion.providedBy(obj)
     convert_document(obj, make_fusion=make_fusion, target_extension=target_extension)
