4cf6c2d03e10b835860b56bf97f2474a9c7f00c1
Thomas Desvenain <thomas.desvenain@gmail.com>
2016-11-17 10:13:57 +0100

    Do not refresh at ContainerModified event

--- src/collective/documentfusion/subscribers.py
+++ src/collective/documentfusion/subscribers.py
@@ -4,10 +4,14 @@ from collective.documentfusion.interfaces import (
     ISettings)
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
+from zope.container.interfaces import IContainerModifiedEvent
 
 
 def refresh(obj, event=None):
     # TODO: get all conversions
+    if IContainerModifiedEvent.providedBy(event):
+        return
+
     settings = getUtility(IRegistry).forInterface(ISettings)
     if settings.auto_refresh_enabled:
         refresh_conversion(obj)
