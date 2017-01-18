c91a0c010ef3cdaa7c46ba05755c604a1e13bc63
Thomas Desvenain <thomas.desvenain@gmail.com>
2017-01-18 09:21:37 +0100

    [WIP] Fix tests

--- src/collective/documentfusion/tests/test_setup.py
+++ src/collective/documentfusion/tests/test_setup.py
@@ -6,6 +6,8 @@ import datetime
 import tempfile
 import subprocess
 
+from plone.uuid.interfaces import IUUID
+from zope.component import getMultiAdapter
 from zope.event import notify
 from zope.component import getUtility
 from zope.interface.declarations import alsoProvides
@@ -20,10 +22,9 @@ from plone.namedfile.file import NamedFile
 from plone.app.blob.adapters.file import BlobbableFile
 
 from collective.documentfusion.testing import IntegrationTestCase
-from collective.documentfusion.interfaces import ICollectiveDocumentfusionLayer
+from collective.documentfusion.interfaces import ICollectiveDocumentfusionLayer, IModelFileSource, IFusionData, IMergeDataSources
 from collective.documentfusion.interfaces import (
     STATUS_STORAGE_KEY, TASK_SUCCEEDED)
-
 
 TEST_LETTER_ODT = os.path.join(os.path.dirname(__file__), 'letter.odt')
 TEST_LABEL_ODT = os.path.join(os.path.dirname(__file__), 'label.odt')
@@ -38,18 +39,87 @@ class TestInstall(IntegrationTestCase):
         self.portal = self.layer['portal']
         self.installer = api.portal.get_tool('portal_quickinstaller')
 
+    def test_named_source(self):
+        request = self.portal.REQUEST
+        alsoProvides(self.portal.REQUEST, ICollectiveDocumentfusionLayer)
+        content = api.content.create(self.portal, type='letter',
+                                     title=u"En réponse...",
+                                     file=NamedFile(data=open(TEST_LETTER_ODT).read(),
+                                                    filename=u'letter.odt',
+                                                    contentType='application/vnd.oasis.opendocument.text'),
+                                     sender_name="Thomas Desvenain",
+                                     sender_address="573 Quai de Turenne",
+                                     recipient_name="Vincent Fretin",
+                                     date=datetime.date(2012, 12, 23))
+        model = getMultiAdapter((content, content.REQUEST), IModelFileSource, name='')()
+        self.assertEqual(model.filename, 'letter.odt')
+
+    def test_fusion_data(self):
+        content = api.content.create(self.portal, type='letter',
+                                     title=u"En réponse...",
+                                     file=NamedFile(data=open(TEST_LETTER_ODT).read(),
+                                                    filename=u'letter.odt',
+                                                    contentType='application/vnd.oasis.opendocument.text'),
+                                     sender_name="Thomas Desvenain",
+                                     sender_address="573 Quai de Turenne",
+                                     recipient_name="Vincent Fretin",
+                                     date=datetime.date(2012, 12, 23))
+        fusion_data = getMultiAdapter((content, content.REQUEST), IFusionData, name='')()
+        expected_fusion_data = {
+            'Author': 'test_user_1_',
+            'Title': u"En réponse...",
+            'date': '2012-12-23',
+            'description': '',
+            'document_id': 'en-reponse',
+            'file': u'letter.odt',
+            'language': '',
+            'path': '/plone/en-reponse',
+            'recipient_address': None,
+            'recipient_name': 'Vincent Fretin',
+            'sender_address': '573 Quai de Turenne',
+            'sender_name': 'Thomas Desvenain',
+            'subjects': u'',
+            'title': u"En réponse...",
+            'uid': IUUID(content),
+            'url': 'http://nohost/plone/en-reponse'
+        }
+        self.assertEqual(fusion_data, expected_fusion_data)
+
+    def test_collection_source(self):
+        # data source is in a related content
+        # we merge two files from two sources
+        alsoProvides(self.portal.REQUEST, ICollectiveDocumentfusionLayer)
+        intids = getUtility(IIntIds)
+        source_1 = api.content.create(self.portal, type='contact_infos',
+                                      id='desvenain_thomas')
+
+        source_2 = api.content.create(self.portal, type='contact_infos',
+                                      id='fretin_vincent')
+
+        content = api.content.create(self.portal, type='label_model',
+                                     title=u"Modèle d'étiquette",
+                                     file=NamedFile(data=open(TEST_LABEL_ODT).read(),
+                                                    filename=u'label.odt',
+                                                    contentType='application/vnd.oasis.opendocument.text'),
+                                     relatedItems=[RelationValue(intids.getId(source_1)),
+                                                   RelationValue(intids.getId(source_2))],
+                                     )
+
+        content_collection = getMultiAdapter((content, content.REQUEST), IMergeDataSources, name='')()
+        self.assertItemsEqual(content_collection, [source_1, source_2])
+
     def test_document_fusion(self):
         # data source and model are in the same content
         alsoProvides(self.portal.REQUEST, ICollectiveDocumentfusionLayer)
         content = api.content.create(self.portal, type='letter',
-                           title=u"En réponse...",
-                           file=NamedFile(data=open(TEST_LETTER_ODT).read(),
-                                          filename=u'letter.odt',
-                                          contentType='application/vnd.oasis.opendocument.text'),
-                           sender_name="Thomas Desvenain",
-                           sender_address="57 Quai du Pré Long",
-                           recipient_name="Vincent Fretin",
-                           date=datetime.date(2012, 12, 23))
+                                     title=u"En réponse...",
+                                     file=NamedFile(data=open(TEST_LETTER_ODT).read(),
+                                                    filename=u'letter.odt',
+                                                    contentType='application/vnd.oasis.opendocument.text'),
+                                     sender_name="Thomas Desvenain",
+                                     sender_address="573 Quai de Turenne",
+                                     recipient_name="Vincent Fretin",
+                                     date=datetime.date(2012, 12, 23))
 
         notify(ObjectModifiedEvent(content))
 
@@ -70,7 +140,7 @@ class TestInstall(IntegrationTestCase):
         subprocess.call(['pdftotext', generated_path, txt_path])
         txt = open(txt_path).read().replace(r'\xc2\xa0', ' ')
         self.assertIn('Vincent Fretin', txt)
-        self.assertIn('57 Quai du Pré Long', txt)
+        self.assertIn('573 Quai de Turenne', txt)
         self.assertIn('2012', txt)
         self.assertIn(u'EN RÉPONSE...', txt)
 
@@ -90,12 +160,12 @@ class TestInstall(IntegrationTestCase):
                                       city='Lille')
 
         content = api.content.create(self.portal, type='label_model',
-                           title=u"Modèle d'étiquette",
-                           file=NamedFile(data=open(TEST_LABEL_ODT).read(),
-                                          filename=u'label.odt',
-                                          contentType='application/vnd.oasis.opendocument.text'),
-                           relatedItems=[RelationValue(intids.getId(source_1))],
-                           )
+                                     title=u"Modèle d'étiquette",
+                                     file=NamedFile(data=open(TEST_LABEL_ODT).read(),
+                                                    filename=u'label.odt',
+                                                    contentType='application/vnd.oasis.opendocument.text'),
+                                     relatedItems=[RelationValue(intids.getId(source_1))],
+                                     )
 
         notify(ObjectModifiedEvent(content))
 
@@ -140,13 +210,13 @@ class TestInstall(IntegrationTestCase):
                                       city='LESQUIN')
 
         content = api.content.create(self.portal, type='label_model',
-                           title=u"Modèle d'étiquette",
-                           file=NamedFile(data=open(TEST_LABEL_ODT).read(),
-                                          filename=u'label.odt',
-                                          contentType='application/vnd.oasis.opendocument.text'),
-                           relatedItems=[RelationValue(intids.getId(source_1)),
-                                         RelationValue(intids.getId(source_2))],
-                           )
+                                     title=u"Modèle d'étiquette",
+                                     file=NamedFile(data=open(TEST_LABEL_ODT).read(),
+                                                    filename=u'label.odt',
+                                                    contentType='application/vnd.oasis.opendocument.text'),
+                                     relatedItems=[RelationValue(intids.getId(source_1)),
+                                                   RelationValue(intids.getId(source_2))],
+                                     )
 
         notify(ObjectModifiedEvent(content))
 
@@ -199,18 +269,18 @@ class TestInstall(IntegrationTestCase):
                            city='LESQUIN')
 
         collection = api.content.create(self.portal, type='Collection',
-                    id='all_labels',
-                    query=[{'i': 'portal_type',
-                            'o': 'plone.app.querystring.operation.selection.is',
-                            'v': ['contact_infos']}])
+                                        id='all_labels',
+                                        query=[{'i': 'portal_type',
+                                                'o': 'plone.app.querystring.operation.selection.is',
+                                                'v': ['contact_infos']}])
 
         content = api.content.create(self.portal, type='label_model',
-                           title=u"Modèle d'étiquette",
-                           file=NamedFile(data=open(TEST_LABEL_ODT).read(),
-                                          filename=u'label.odt',
-                                          contentType='application/vnd.oasis.opendocument.text'),
-                           relatedItems=[RelationValue(intids.getId(collection))],
-                           )
+                                     title=u"Modèle d'étiquette",
+                                     file=NamedFile(data=open(TEST_LABEL_ODT).read(),
+                                                    filename=u'label.odt',
+                                                    contentType='application/vnd.oasis.opendocument.text'),
+                                     relatedItems=[RelationValue(intids.getId(collection))],
+                                     )
 
         notify(ObjectModifiedEvent(content))
 
@@ -251,15 +321,15 @@ class TestInstall(IntegrationTestCase):
                                    )
 
         content = api.content.create(self.portal, type='invoice',
-                           title=u"Invoice for the Great Company intranet",
-                           bill_date=datetime.date(2012, 12, 23),
-                           customer="The Great company",
-                           order_num='12A',
-                           vat_excluded=1000.0,
-                           vat=0.19,
-                           vat_included=1190.0,
-                           relatedItems=[RelationValue(intids.getId(model))],
-                           )
+                                     title=u"Invoice for the Great Company intranet",
+                                     bill_date=datetime.date(2012, 12, 23),
+                                     customer="The Great company",
+                                     order_num='12A',
+                                     vat_excluded=1000.0,
+                                     vat=0.19,
+                                     vat_included=1190.0,
+                                     relatedItems=[RelationValue(intids.getId(model))],
+                                     )
 
         content.unrestrictedTraverse('@@documentfusion-refresh')()
         notify(ObjectModifiedEvent(content))
